8927648bb62d4651a178de165a89a8872dc81afc
Alicia Yantz <aliciayantz@ScottCilsiPhone.localdomain>
2019-02-15 13:49:24 -0800

    finished function

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,14 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function oddOrEven(number){
+  if(number%2 === 0){
+    return "Even"
+  } else {
+    return "Odd"
+  }
+    console.log(oddOrEven)
 }
 
-oddOrEven(4)
-oddOrEven(11)
-oddOrEven(75)
+console.log(oddOrEven(4))
+console.log(oddOrEven(11))
+console.log(oddOrEven(75))
